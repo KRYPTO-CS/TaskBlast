e35868838b0dba2c80c510b3ccf82f8af789428c
DanielGar12 <garcia.daniel23256@gmail.com>
2026-02-19 10:47:48 -0600

    test

--- TaskBlast/app/index.tsx
+++ TaskBlast/app/index.tsx
@@ -1,11 +1,16 @@
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18next";
 import Login from "./pages/Login";
+import { CoachmarkProvider, CoachmarkOverlay } from '@edwardloopez/react-native-coachmark';
+
 
 export default function Index() {
   return (
     <I18nextProvider i18n={i18n}>
-      <Login />
+      <CoachmarkProvider>
+        <Login />
+        <CoachmarkOverlay />
+      </CoachmarkProvider>
     </I18nextProvider>
   );
 }
